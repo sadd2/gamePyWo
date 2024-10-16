402db31a3b3ac211d5e68455fafd76c19e94cc1d
sadd2 <70380463+sadd2@users.noreply.github.com>
2024-10-17 00:00:00 +0500

    'add file rules.py'

---
+++ rules.py
@@ -0,0 +1,9 @@
+# rules.py
+class Rules:
+    @staticmethod
+    def show_rules():
+        print("Game Rules:")
+        print("1. You will be asked to guess a number between 1 and 100.")
+        print("2. After each guess, you will receive feedback on whether your guess is too high, too low, or correct.")
+        print("3. Try to guess the number in as few attempts as possible.")
+        print("4. Good luck!")
